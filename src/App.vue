72b55bfee7a9600923318e39e8bf7071a7c755f5
Jure Beton <jure.beton@gmail.com>
2020-05-02 17:08:00 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dropdown/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dropdown from "./components/Dropdown.vue"
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Dropdown
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'Lato', sans-serif;
+  background-color: #EDF2F7;
 }
 </style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="select-box">
+      <div class="options-container">
+        <div class="option" v-on:click="selectDrop" v-for="item in vegetables" :key="item.name">
+          <input type="radio" class="radio" name="category" />
+          <label>{{item.name}}</label>
+        </div>
+      </div>
+      <div class="selected" v-on:click="drop">Select an Item</div>
+      <div class="search-box">
+        <input type="text" v-on:keyup="search" autofocus placeholder="This is a search input..." />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+  components: {},
+  data() {
+    return {
+        searchInput: "",
+        vegetables: [
+        { name: "Broccoli" },
+        { name: "Lettuce" },
+        { name: "Peppers" },
+        { name: "Cucumber" },
+        { name: "Parsley" },
+        { name: "Root vegetables" }
+      ]
+    };
+  },
+  methods: {
+    drop: e => {
+      document.querySelector(".options-container").classList.toggle("active");
+      e.target.classList.toggle("border");
+      
+      
+    },
+    selectDrop: e => {
+      document.querySelector(".selected").innerHTML = e.target.innerHTML;
+    },
+    search: e => {
+      let selected = document.querySelector(".selected");
+      let searchTerm = e.target.value;
+      selected.innerHTML = searchTerm;
+      if (selected.innerHTML == "") {
+        selected.textContent = "Select an Item";
+      }
+    }
+  }
+};
+</script>
+
+<style src="../assets/css/Dropdown.style.css">
+</style>
